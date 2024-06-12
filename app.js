9a59e41ed4b43afb76691bbe38b90206432f1f27
meghana-wq <meghanakh000@gmail.com>
2024-06-12 23:28:23 +0530

    backend view completed

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const app = express()
 app.use(cors())
 
 const recipes  = require("./models/recipe")
-const {reviewModel}=require("./models/recipe")
+const {recipeModel}=require("./models/recipe")
 
 app.use(express.json())
 mongoose.connect("mongodb+srv://pachu123:pachu000@cluster0.bd7lkke.mongodb.net/recipedb?retryWrites=true&w=majority&appName=Cluster0")
 
 app.post("/add", (req, res) => {
     let input = req.body
-    let recipe=new recipes.recipeModel(input)
+    let recipe=new recipeModel(input)
     //console.log(recipe)
     //console.log(input)
     // res.send("Add page")
@@ -23,6 +23,17 @@ app.post("/add", (req, res) => {
 
 app.get("/add", (req, res) => {
     res.send("add page")
+})
+app.post("/view", (req, res) => {
+    recipeModel.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch((error)=>{
+        res.json(error)
+    }
+)
+    //res.send("view page")
 })
 app.listen(8080, () => {
     console.log("server started")
